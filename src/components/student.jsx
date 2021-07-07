b0ba7b16acfbf69a79e28481f6b99b73bc211347
Stefan Her <stefan.her@gmail.com>
2021-07-07 13:31:15 +0200

    import classnames -> condition dans l'obj cs

--- src/components/student.jsx
+++ src/components/student.jsx
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
+import style from './students.module.css';
+import classNames from 'classnames';
 
 const Student = (props) => {
     const {firstname, lastname, option, sexe, result} = props;
+    let cs = classNames({[style.student]: true}, {[style.fail]: (result < 50) ? true : false});
 
     return(
-        <div>
+        <div className={cs}>
             <h1>
                 {firstname.charAt(0).toUpperCase() + firstname.substring(1).toLowerCase()} {lastname.charAt(0).toUpperCase() + lastname.substring(1).toLowerCase()}
             </h1>
             <p>{sexe}</p>
             <h2>{option}</h2>
-            <p>{result}</p>
+            <p className={style.result}>{result}</p>
         </div>
     );
 }
